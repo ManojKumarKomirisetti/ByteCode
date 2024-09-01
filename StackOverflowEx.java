219987163ad4928a5d39218af69e2cdce63dcd60
ManojKumarKomirisetti <manojkumarkomirisetti@gmail.com>
2024-09-01 23:41:31 +0530

    1/9/24

---
+++ StackOverflowEx.java
@@ -0,0 +1,16 @@
+public class StackOverflowEx {
+
+    public static void recursionEx()
+    {
+        recursionEx();
+        
+    }
+    public static void main(String Args[])
+    {
+        recursionEx();
+
+       
+
+    }
+    
+}
